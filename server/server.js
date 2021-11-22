2bdd8d52626ec608da73b48f558b8afb262003a3
Tammy Gagliano <tracsd@gmail.com>
2021-11-22 14:03:40 -0600

    update port number

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 // Import the Apollo Server
 const { ApolloServer } = require('apollo-server-express');
