3edd9c0a9112b1f5b50fd95c9be20f242ebba285
Tammy Gagliano <tracsd@gmail.com>
2021-11-21 21:20:19 -0600

    update server file with Apollo and GraphQL

--- server/server.js
+++ server/server.js
@@ -3,9 +3,27 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Import the Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+// Import the typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
+// Set up express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up the apollo server and pass in the schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// Integrate the Apollo server and pass in the schema
+server.applyMiddleware({ app });
+
+// Express middleware for parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +32,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// GraphQL and express server start
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,7 +4,15 @@ const jwt = require('jsonwebtoken');
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
+//Apollo server authentication
 module.exports = {
+  signToken: function({ username, email, _id }) {
+    // create a token with the username, email, and id, signed with the secret
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -19,21 +27,19 @@ module.exports = {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
-    // verify token and get user data out of it
-    try {
+     // if there is no token, return the request object as sent
+     if (!token) {
+      return req;
+  }
+  // otherwise...
+  try {
+      // decode and attach user data to the request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
+  } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  }
+  // return the updated request object
+  return req;
+}
 };
