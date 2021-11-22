9ce1a4d07de3c63ea6875196dc5b1ff1434f400b
Tammy Gagliano <tracsd@gmail.com>
2021-11-21 21:08:47 -0600

    clear errors

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
-("");
+
 const client = new ApolloClient({
   request: (operation) => {
     const token = localStorage.getItem("id_token");
